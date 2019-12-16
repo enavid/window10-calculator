5ef4f1338ddd9fb15d5d8f219c7493d44d4a549d
en.navid <en.navid@outlook.com>
2019-12-16 13:31:29 +0330

    create server

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs   = require('fs');
+const path = require('path');
+
+const server = http.createServer((request, response) =>{
+    let routes = {
+        'GET': {
+            '/': indexHtml,
+        }
+    }
+
+    let handler = routes[request.method][request.url];
+
+    handler = handler || readFile;
+    handler(request, response);
+
+});
+
+server.listen(3000);
+
+function indexHtml(request, response) {
+    readFile({url: 'index.html'}, response);
+}
+
+function readFile(request, response){
+    var filePath = path.join(__dirname, '../client/view', request.url);
+
+    fs.readFile(filePath, (error, data) => {
+        if (error) {
+            response.writeHead(404);
+            response.write('Not found error 404');
+            response.end()
+        } else{
+            response.writeHead(200);
+            response.write(data);
+            response.end();
+        }   
+    })
+}
