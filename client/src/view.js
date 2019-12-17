84922bc8c94ec887ba03ad28169bf6a285f9c196
en.navid <en.navid@outlook.com>
2019-12-17 12:14:44 +0330

    add filter function and use it for keyboard input

--- client/src/view.js
+++ client/src/view.js
@@ -10,11 +10,21 @@ buttons.onclick = (e) => {
 // read keyboard 
 document.addEventListener('keydown',(e) =>{
    //e.preventDefault();
-    var data = e.key;
+    var data = inputFilter(e.key);
+    if (data == undefined) return;
+
     writeToDisplay(data);
 
 })
 
 function writeToDisplay(data){
     displayText.innerHTML = data;
+}
+
+
+function inputFilter(data){
+    var validData = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '-', '+'];
+    if(validData.indexOf(data) !== -1){
+        return data;
+    }
 }
