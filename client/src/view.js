d7016c532cff7cae6b7bbd18e4d3169a0f3dcd24
en.navid <en.navid@outlook.com>
2019-12-17 11:58:45 +0330

    Add function for read input and display in p tag

--- client/src/view.js
+++ client/src/view.js
@@ -1,2 +1,20 @@
+const displayText = document.getElementById('display-text');
+const buttons = document.getElementById('keyboard-buttons');
 
+// read web input
+buttons.onclick = (e) => {
+    var data = e.target.getAttribute('value');
+    writeToDisplay(data) ;
+};
 
+// read keyboard 
+document.addEventListener('keydown',(e) =>{
+   //e.preventDefault();
+    var data = e.key;
+    writeToDisplay(data);
+
+})
+
+function writeToDisplay(data){
+    displayText.innerHTML = data;
+}
