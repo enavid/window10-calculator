a035cbbdd0a6ffa5d81b6958165ded62514a9738
en.navid <en.navid@outlook.com>
2019-12-17 12:53:07 +0330

    call controll init

--- client/src/app.js
+++ client/src/app.js
@@ -1,14 +1,1 @@
-const displayText = document.getElementById('display-text');
-const buttons = document.getElementById('keyboard-buttons')
-
-// read web input
-buttons.onclick = (e) => {
-    console.log(e.target.getAttribute('value'));
-};
-
-// read keyboard 
-document.addEventListener('keydown',(e) =>{
-   //e.preventDefault();
-    console.log(e.key)
-
-})
+controller.init();
