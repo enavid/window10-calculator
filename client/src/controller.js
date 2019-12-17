4c0be658a4ce9e54cf02a8a944052bd3213b166f
en.navid <en.navid@outlook.com>
2019-12-17 14:16:53 +0330

    Add input Handler

--- client/src/controller.js
+++ client/src/controller.js
@@ -1,20 +1,44 @@
-var controller = function(){
+var controller = function () {
+    var _input = '';
+    var _viewInput = '';
+    var _model = [];
+
 
     return {
         init,
     }
     //=======================Initialize=============================
-    function init(){
-        view.init({inputFilter,});
+    function init() {
+        view.init({ inputFilter, inputHandler });
     }
 
     //=======================Functions=============================
 
-    function inputHandler(){
+    function inputHandler(data) {
+        var sign = ['+', '-', '*', '/'];
+        var result = sign.indexOf(data);
+
+        result == -1 ?  createStringNumber(data) : createNumber(sign[result]);  
+        
+        _viewInput = _viewInput + data ;
+        view.writeToDisplay(_viewInput);
 
     }
+    function createStringNumber(data) {
+        _input = _input + data;
+    }
+    function createNumber(sign) {
+
+        if(_input === ''){
+            _model.push(sign);
+        }else{
+            _model.push(parseFloat(_input));
+            _model.push(sign);
+            _input = '';
+        }
+    }
     function inputFilter(data) {
-        var validData = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '-', '+'];
+        var validData = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '-', '+', '.'];
         if (validData.indexOf(data) !== -1) {
             return data;
         }
--- client/src/view.js
+++ client/src/view.js
@@ -2,26 +2,29 @@ const displayText = document.getElementById('display-text');
 const buttons = document.getElementById('keyboard-buttons');
 
 var view = function () {
+    var _listeners = {};
 
     return {
         init,
+        writeToDisplay,
     }
     //=======================Initialize=============================
 
-    function init({inputFilter}) {
+    function init({inputFilter, inputHandler}) {
+
 
         // read web input
         buttons.onclick = (e) => {
             var data = e.target.getAttribute('value');
-            writeToDisplay(data);
+            inputHandler(data);
         };
 
-        // read keyboard 
+        // read keyboard input
         document.addEventListener('keydown', (e) => {
             //e.preventDefault();
             var data = inputFilter(e.key);
             if (data == undefined) return;
-            writeToDisplay(data);
+            inputHandler(data);
         })
     }
 
