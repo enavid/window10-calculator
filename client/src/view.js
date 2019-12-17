bff98322c5bff032dd367763081bdec424559823
en.navid <en.navid@outlook.com>
2019-12-17 12:50:20 +0330

    create module view

--- client/src/view.js
+++ client/src/view.js
@@ -1,30 +1,39 @@
 const displayText = document.getElementById('display-text');
 const buttons = document.getElementById('keyboard-buttons');
 
-// read web input
-buttons.onclick = (e) => {
-    var data = e.target.getAttribute('value');
-    writeToDisplay(data) ;
-};
+var view = function () {
 
-// read keyboard 
-document.addEventListener('keydown',(e) =>{
-   //e.preventDefault();
-    var data = inputFilter(e.key);
-    if (data == undefined) return;
-
-    writeToDisplay(data);
-
-})
-
-function writeToDisplay(data){
-    displayText.innerHTML = data;
-}
-
-
-function inputFilter(data){
-    var validData = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '-', '+'];
-    if(validData.indexOf(data) !== -1){
-        return data;
+    return {
+        init,
     }
-}
+
+    //========================================================
+
+    function init() {
+        // read web input
+        buttons.onclick = (e) => {
+            var data = e.target.getAttribute('value');
+            writeToDisplay(data);
+        };
+
+        // read keyboard 
+        document.addEventListener('keydown', (e) => {
+            //e.preventDefault();
+            var data = inputFilter(e.key);
+            if (data == undefined) return;
+            writeToDisplay(data);
+        })
+
+    }
+
+    function writeToDisplay(data) {
+        displayText.innerHTML = data;
+    }
+
+    function inputFilter(data) {
+        var validData = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '-', '+'];
+        if (validData.indexOf(data) !== -1) {
+            return data;
+        }
+    }
+}();
